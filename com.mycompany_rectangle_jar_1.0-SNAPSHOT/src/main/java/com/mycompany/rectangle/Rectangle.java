cfd5c0b25a86ffd75bf9a8d46e98b8aee7d5220c
gersh theron <44289187+gershtron@users.noreply.github.com>
2023-03-27 12:59:15 +0200

    Add files via upload

---
+++ com.mycompany_rectangle_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/rectangle/Rectangle.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.rectangle;
+
+/**
+ *
+ * @author tn
+ */
+public class Rectangle {
+
+    public static void main(String[] args) {
+        NewJFrame f=new NewJFrame();
+        f.setVisible(true);
+      
+    }
+}
